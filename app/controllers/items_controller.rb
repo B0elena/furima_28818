b0b00a9a19d8a6dd90afd0907223bdf7d535c251
B0elena <e.mc2.1012md@gmail.com>
2020-08-22 19:49:12 +0900

    商品出品機能のバリベーションとアソシエーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,17 @@ class ItemsController < ApplicationController
   def index
     
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :product, :description, :category_id, :condition_id, :shipping_cost_id, :shipping_origin_id, :shipping_day_id, :price,)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  validates :image, :product, :description, :category_id, :condition_id, :shipping_cost_id, :shipping_origin_id, :shipping_day_id, :price, presence: true
 end
