c296854a79c3c2fa7a1e97f09ffae99e9a8ed05d
B0elena <e.mc2.1012md@gmail.com>
2020-08-22 20:39:46 +0900

    プルダウンでカテゴリーを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   validates :image, :product, :description, :category_id, :condition_id, :shipping_cost_id, :shipping_origin_id, :shipping_day_id, :price, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  validates :category_id, numericality: { other_than: 1 }
 end
