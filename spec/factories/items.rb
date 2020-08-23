ff46f7fe1ddc6123b8da64ccb630917713f28a81
B0elena <e.mc2.1012md@gmail.com>
2020-08-23 16:46:38 +0900

    エラーコードのexampleの書き出し

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image              {"a"}
+    product            {"商品名"}
+    description        {"商品説明"}
+    category_id        {"2"}
+    condition_id       {"2"}
+    shipping_cost_id   {"2"}
+    shipping_origin_id {"2"}
+    shipping_day_id    {"2"}
+    price              {"300"}
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
-
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Item do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '商品出品機能' do
+    context '商品出品ができる時' do
+      it "必須項目が全て存在すれば出品できる" do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品ができない時' do
+      it "出品画像が空だと出品できない" do
+      end
+      it "商品名が空だと出品できない" do
+      end
+      it "商品説明が空だと出品できない" do
+      end
+      it "カテゴリーの情報が空だと出品できない" do
+      end
+      it "商品の状態の情報が空だと出品できない" do
+      end
+      it "配送料の負担の情報が空だと出品できない" do
+      end
+      it "発送元の地域の情報が空だと出品できない" do
+      end
+      it "発送日までの日数の情報が空だと出品できない" do
+      end
+      it "価格が空だと出品できない" do
+      end
+      it "価格が ¥300~¥9,999,999 の間でないと出品できない" do
+      end
+      it "価格が半角数字でないと出品できない" do
+      end
+    end
+  end
 end
