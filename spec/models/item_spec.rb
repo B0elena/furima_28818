1816d31bfdb4ba66b309073c7ebc33d5cf5650c8
B0elena <e.mc2.1012md@gmail.com>
2020-08-23 18:08:03 +0900

    エラーコード実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,26 +11,64 @@ describe Item do
     end
     context '商品出品ができない時' do
       it "出品画像が空だと出品できない" do
+        @item.image = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it "商品名が空だと出品できない" do
+        @item.product = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product can't be blank")
       end
       it "商品説明が空だと出品できない" do
+        @item.description = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it "カテゴリーの情報が空だと出品できない" do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it "商品の状態の情報が空だと出品できない" do
+        @item.condition_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it "配送料の負担の情報が空だと出品できない" do
+        @item.shipping_cost_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
       end
       it "発送元の地域の情報が空だと出品できない" do
+        @item.shipping_origin_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping origin can't be blank")
       end
       it "発送日までの日数の情報が空だと出品できない" do
+        @item.shipping_day_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
       end
       it "価格が空だと出品できない" do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it "価格が ¥300~¥9,999,999 の間でないと出品できない" do
+      it "価格が¥300以上でないと出品できない" do
+        @item.price = "299"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "価格が¥9,999,999以下でないと出品できない" do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it "価格が半角数字でないと出品できない" do
+        @item.price = "３００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
     end
   end
