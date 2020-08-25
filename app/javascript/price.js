471412ff0cc223bc21baa3e641709e96e1f7e82d
B0elena <e.mc2.1012md@gmail.com>
2020-08-25 16:58:19 +0900

    Revert "商品がなければダミーを表示する条件分岐の実装"
    This reverts commit cad9e26cda539c86635e9a310a38b1581b350b4c.

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,13 +3,10 @@ function price() {
   const itemprice = document.getElementById("item-price")
   const addtaxprice = document.getElementById("add-tax-price")
   const profit = document.getElementById("profit")
-
-  itemprice.addEventListener('input', function() {
+  itemprice.addEventListener('input', function(){
     const values = itemprice.value
-    valuess = values * 0.1
-    addtaxprice.innerHTML = Math.round(valuess)
+    addtaxprice.innerHTML = values * 0.1
     profit.innerHTML = values - addtaxprice.innerHTML
-  })
+})
 }
-
-window.addEventListener('turbolinks:load', price)
+window.addEventListener('load', price)
