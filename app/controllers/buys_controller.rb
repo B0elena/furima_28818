a8d8b9f823c720f9ab7ef54b2be6827277644a67
B0elena <e.mc2.1012md@gmail.com>
2020-08-27 21:57:17 +0900

    購入する商品の表示

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,8 +1,8 @@
 class BuysController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
+  has_many :buys
 
   PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/.freeze
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
