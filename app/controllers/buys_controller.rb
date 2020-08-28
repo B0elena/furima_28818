8b3b1a2be121abd076c012f61ed72c2c981b6c7a
B0elena <e.mc2.1012md@gmail.com>
2020-08-28 15:57:43 +0900

    クレジットカードでの購入の処理

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -4,5 +4,30 @@ class BuysController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @buy = Buy.new(buy_params)
+    if @buy.valid?
+      pay_item
+      @buy.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
+
+  private
+
+  def buy_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  #秘密鍵を環境変数で
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: params[:token],
+      currency:'jpy'
+    )
+  end
+
 end
